34f873db72eba9b77f174e4a0a9d4a76d0bd9339
Ruediger Meier <ruediger.meier@ga-group.nl>
2010-06-05 12:54:34 +0200

    twsDL, move helper classes to tws_meta.h

--- src/tws_meta.h
+++ src/tws_meta.h
@@ -1,6 +1,52 @@
 #ifndef TWS_META_H
 #define TWS_META_H
 
+#include "ibtws/Contract.h"
+
+#include <QtCore/QString>
+#include <QtCore/QList>
 
 
+
+
+namespace Test {
+
+
+
+
+class HistRequest
+{
+	public:
+		bool fromString( const QString& );
+		QString toString() const;
+		void clear();
+		
+		IB::Contract ibContract;
+		QString endDateTime;
+		QString whatToShow;
+};
+
+
+
+
+
+
+
+
+class WorkTodo
+{
+	public:
+		int fromFile( const QString & fileName );
+		void dump( FILE *stream ) const;
+		
+		QList<HistRequest> histRequests;
+		
+	private:
+		int read_file( const QString & fileName, QList<QByteArray> *list ) const;
+};
+
+
+
+
+} // namespace Test
 #endif
