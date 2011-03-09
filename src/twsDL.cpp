26d723c24f5a0af05e7e0ff12825e375a04bdf45
Ruediger Meier <ruediger.meier@ga-group.nl>
2011-03-09 10:50:35 +0100

    twsDL, move main() to twsDL.cpp

--- src/twsDL.cpp
+++ src/twsDL.cpp
@@ -13,7 +13,8 @@
 #include <QtCore/QRegExp>
 #include <QtCore/QStringList>
 #include <QtCore/QFile>
-
+#include <QtCore/QCoreApplication>
+#include <stdio.h>
 
 
 
@@ -971,3 +972,30 @@ bool PropTwsDL::readProperties()
 ///////////////////////////////////////////////////////////////////////////////
 
 
+
+
+int main(int argc, char *argv[])
+{
+	QCoreApplication app( argc, argv );
+	
+	if( argc > 3 ) {
+		fprintf( stderr, "Usage: %s [configuration file] [worktodo file]\n", argv[0] );
+		return 2;
+	}
+	QString workfile;
+	if( argc == 3 ) {
+		workfile = argv[2];
+	}
+	TwsDL twsDL( argc >= 2 ? argv[1] : "twsDL.cfg", workfile );
+	twsDL.start();
+	
+	TwsDL::State state = twsDL.currentState();
+	Q_ASSERT( (state == TwsDL::QUIT_READY) ||
+		(state == TwsDL::QUIT_ERROR) );
+	if( state == TwsDL::QUIT_READY ) {
+		return 0;
+	} else {
+		qDebug() << "Finished with errors.";
+		return 1;
+	}
+}
