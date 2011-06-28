03086de65db1fe4069626d3789a83eb323d39753
Ruediger Meier <ruediger.meier@ga-group.nl>
2011-06-28 12:31:58 +0200

    cmd parsing libpopt

--- src/twsDL.cpp
+++ src/twsDL.cpp
@@ -15,7 +15,96 @@
 #include <QtCore/QStringList>
 #include <QtCore/QFile>
 #include <QtCore/QCoreApplication>
+#include <popt.h>
 #include <stdio.h>
+
+
+static poptContext opt_ctx;
+static const char *workfilep = "";
+static const char *configfilep = "twsDL.cfg";
+
+#define VERSION_MSG \
+PACKAGE_NAME " " PACKAGE_VERSION "\n\
+Copyright (C) 2010-2011 Ruediger Meier <sweet_f_a@gmx.de>\n\
+License: BSD 3-Clause\n"
+
+
+static void displayArgs( poptContext con, poptCallbackReason /*foo*/,
+	poptOption *key, const char */*arg*/, void */*data*/ )
+{
+	if (key->shortName == 'h') {
+		poptPrintHelp(con, stdout, 0);
+	} else if (key->shortName == 'V') {
+		fprintf(stdout, VERSION_MSG);
+	} else {
+		poptPrintUsage(con, stdout, 0);
+	}
+	
+	exit(0);
+}
+
+static struct poptOption flow_opts[] = {
+	POPT_TABLEEND
+};
+
+static struct poptOption help_opts[] = {
+	{NULL, '\0', POPT_ARG_CALLBACK, (void*)displayArgs, 0, NULL, NULL},
+	{"help", 'h', POPT_ARG_NONE, NULL, 0, "Show this help message.", NULL},
+	{"version", 'V', POPT_ARG_NONE, NULL, 0, "Print version string and exit.",
+		NULL},
+	{"usage", '\0', POPT_ARG_NONE, NULL, 0, "Display brief usage message."
+		, NULL},
+	POPT_TABLEEND
+};
+
+static const struct poptOption twsDL_opts[] = {
+	{NULL, '\0', POPT_ARG_INCLUDE_TABLE, flow_opts, 0,
+	 "Program advice:", NULL},
+	{NULL, '\0', POPT_ARG_INCLUDE_TABLE, help_opts, 0,
+	 "Help options:", NULL},
+	POPT_TABLEEND
+};
+
+void clear_popt()
+{
+	poptFreeContext(opt_ctx);
+}
+
+void twsDL_parse_cl(size_t argc, const char *argv[])
+{
+	opt_ctx = poptGetContext(NULL, argc, argv, twsDL_opts, 0);
+	atexit(clear_popt);
+	
+	poptSetOtherOptionHelp( opt_ctx, "[OPTION]... [CONFIG_FILE] [WORK_FILE]");
+	
+	int rc;
+	while( (rc = poptGetNextOpt(opt_ctx)) > 0 ) {
+		// handle options when we have returning ones
+		Q_ASSERT(false);
+	}
+	
+	if( rc != -1 ) {
+		fprintf( stderr, "error: %s '%s'\n",
+			poptStrerror(rc), poptBadOption(opt_ctx, 0) );
+		exit(2);
+	}
+	
+	const char** rest = poptGetArgs(opt_ctx);
+	if( rest != NULL ) {
+		if( *rest != NULL ) {
+			configfilep = *rest;
+			rest++;
+		}
+		if( *rest != NULL ) {
+			workfilep = *rest;
+			rest++;
+		}
+		if( *rest != NULL ) {
+			fprintf( stderr, "error: bad usage\n" );
+			exit(2);
+		}
+	}
+}
 
 
 
@@ -984,15 +1073,9 @@ int main(int argc, char *argv[])
 {
 	QCoreApplication app( argc, argv );
 	
-	if( argc > 3 ) {
-		fprintf( stderr, "Usage: %s [configuration file] [worktodo file]\n", argv[0] );
-		return 2;
-	}
-	QString workfile;
-	if( argc == 3 ) {
-		workfile = argv[2];
-	}
-	TwsDL twsDL( argc >= 2 ? argv[1] : "twsDL.cfg", workfile );
+	twsDL_parse_cl(argc, (const char **) argv);
+	
+	TwsDL twsDL( configfilep, workfilep );
 	twsDL.start();
 	
 	TwsDL::State state = twsDL.currentState();
