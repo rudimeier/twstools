7a3bb15e6b801180b3927d2ae7e484c255c293ad
Ruediger Meier <ruediger.meier@ga-group.nl>
2011-08-16 16:30:54 +0200

    twsgen, add filter option --max-expiry (experimental, incomplete)

--- src/twsgen.cpp
+++ src/twsgen.cpp
@@ -65,6 +65,7 @@ static int utcp = 0;
 static const char *includeExpiredp = "auto";
 static int to_csvp = 0;
 static int no_convp = 0;
+static const char *max_expiryp = NULL;
 
 
 static char** wts_list = NULL;
@@ -135,6 +136,9 @@ static void gengetopt_check_opts()
 	}
 	to_csvp = args_info.to_csv_given;
 	no_convp = args_info.no_conv_given;
+	if( args_info.max_expiry_given ) {
+		max_expiryp = args_info.max_expiry_arg;
+	}
 }
 
 static void gengetopt_free()
@@ -381,16 +385,37 @@ bool gen_csv()
 	if( ! file.openFile(filep) ) {
 		return false;
 	}
+	
+	if( max_expiryp != NULL ) {
+		struct tm tm_tmp;
+		memset(&tm_tmp, 0, sizeof(struct tm));
+		if( ib_strptime( &tm_tmp, max_expiryp ) == -1 ) {
+			fprintf( stderr, "error, "
+				"max-expiry must be IB's format YYYYMMDD.\n" );
+			return false;
+		}
+		DEBUG_PRINTF("skipping expiries newer than: '%s'", max_expiryp );
+	}
+	
 	xmlNodePtr xn;
 	int count_docs = 0;
 	while( (xn = file.nextXmlNode()) != NULL ) {
 		count_docs++;
 		PacketHistData *phd = PacketHistData::fromXml( xn );
+		
+		if( max_expiryp != NULL ) {
+			const std::string &expiry = phd->getRequest().ibContract().expiry;
+			if( !expiry.empty() && strcmp(max_expiryp, expiry.c_str() ) < 0 ) {
+				goto cont;
+			}
+		}
+		
 		if( !no_convp ) {
 			phd->dump( true /* printFormatDates */);
 		} else {
 			phd->dumpXml();
 		}
+cont:
 		delete phd;
 	}
 	fprintf( stderr, "notice, %d xml docs parsed from file '%s'\n",
