bb37415491435c890cbe82667c476e3f00bc8407
Ruediger Meier <ruediger.meier@ga-group.nl>
2013-02-15 22:35:14 +0100

    always use eConnect2, don't define TWSAPI_IPV6

--- src/tws_client.cpp
+++ src/tws_client.cpp
@@ -97,11 +97,7 @@ bool TWSClient::connectTWS( const std::string &host, int port, int clientId,
 {
 	DEBUG_PRINTF("connect: %s:%d, clientId: %d", host.c_str(), port, clientId);
 	
-#if defined TWSAPI_IPV6
 	return ePosixClient->eConnect2( host.c_str(), port, clientId, ai_family );
-#else
-	return ePosixClient->eConnect( host.c_str(), port, clientId );
-#endif
 }
 
 
