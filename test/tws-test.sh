ba0ecad55ea683eb3a49ce4ed74c48a9ef3bf2e6
Ruediger Meier <ruediger.meier@ga-group.nl>
2013-02-18 14:26:51 +0100

    test suite, copy over improved test suite from atem
    Now our test suite equals to atem 55e2a585. The tests still need to be fixed.

--- test/tws-test.sh
+++ test/tws-test.sh
@@ -3,11 +3,10 @@
 usage()
 {
 	cat <<EOF 
-$(basename ${0}) [OPTION] TEST_FILE
+`basename ${0}` [OPTION] TEST_FILE
 
 --builddir DIR  specify where tools can be found
 --srcdir DIR    specify where the source tree resides
---hash PROG     use hasher PROG instead of md5sum
 --husk PROG     use husk around tool, e.g. 'valgrind -v'
 
 -h, --help      print a short help screen
@@ -33,7 +32,7 @@ for arg; do
 		shift 2
 		;;
 	"--husk")
-		HUSK="${2}"
+		husk="${2}"
 		shift 2
 		;;
 	--)
@@ -42,8 +41,8 @@ for arg; do
 		break
 		;;
 	"-"*)
-		echo "unknown option ${arg}" >&2
-		shift
+		echo "`basename ${0}`: unknown option '${arg}'" >&2
+		exit 1
 		;;
 	*)
 		testfile="${1}"
@@ -51,70 +50,91 @@ for arg; do
 	esac
 done
 
-## setup
-fail=0
-tool_stdout=$(mktemp "/tmp/tmp.XXXXXXXXXX")
-tool_stderr=$(mktemp "/tmp/tmp.XXXXXXXXXX")
+if test -z "${testfile}"; then
+	echo "`basename ${0}`: no test file given" >&2
+	exit 1
+fi
 
-## source the check
-. "${testfile}" || fail=1
-
-rm_if_not_src()
-{
-	file="${1}"
-	srcd="${2:-${srcdir}}"
-	dirf=$(dirname "${file}")
-
-	if test "${dirf}" -ef "${srcd}"; then
-		## treat as precious source file
-		:
-	elif test "$(pwd)" -ef "${srcd}"; then
-		## treat as precious source file
-		:
-	else
-		rm -vf -- "${file}"
-	fi
-}
-
-myexit()
-{
-	rm_if_not_src "${stdin}" "${srcdir}"
-	rm_if_not_src "${stdout}" "${srcdir}"
-	rm_if_not_src "${stderr}" "${srcdir}"
-	rm -f -- "${tool_stdout}" "${tool_stderr}"
-	exit ${1:-1}
-}
-
+## some helper funs
 xrealpath()
 {
 	readlink -f "${1}" 2>/dev/null || \
 	realpath "${1}" 2>/dev/null || \
-	( cd "$(dirname "${1}")" || exit 1
-		tmp_target="$(basename "${1}")"
+	( cd "`dirname "${1}"`" || exit 1
+		tmp_target="`basename "${1}"`"
 		# Iterate down a (possible) chain of symlinks
 		while test -L "${tmp_target}"; do
-			tmp_target="$(readlink "${tmp_target}")"
-			cd "$(dirname "${tmp_target}")" || exit 1
-			tmp_target="$(basename "${tmp_target}")"
+			tmp_target="`readlink "${tmp_target}"`"
+			cd "`dirname "${tmp_target}"`" || exit 1
+			tmp_target="`basename "${tmp_target}"`"
 		done
-		echo "$(pwd -P || pwd)/${tmp_target}"
+		echo "`pwd -P || pwd`/${tmp_target}"
 	) 2>/dev/null
 }
 
-find_file()
+ts_sha1sum()
 {
-	file="${1}"
+	local file="${1}"
+	local tmp
 
-	if test -z "${file}"; then
-		:
-	elif test -r "${file}"; then
-		echo "${file}"
-	elif test -r "${builddir}/${file}"; then
-		xrealpath "${builddir}/${file}"
-	elif test -r "${srcdir}/${file}"; then
-		xrealpath "${srcdir}/${file}"
+	if ! test -r "${file}"; then
+		echo "ts_sha1sum: could not read file '${file}'" >&2
+		return 1
 	fi
+
+	if tmp="`sha1sum "${file}" 2>/dev/null`"; then
+		echo "${tmp}" | (read sum rest; echo "${sum}")
+	elif tmp="`shasum "${file}" 2>/dev/null`"; then
+		echo "${tmp}" | (read sum rest; echo "${sum}")
+	elif tmp="`sha1 -n "${file}" 2>/dev/null`"; then
+		echo "${tmp}" | (read sum rest; echo "${sum}")
+	elif tmp="`sha1 -q "${file}" 2>/dev/null`"; then
+		echo "${tmp}"
+	else
+		echo "ts_sha1sum: unable to calculate sha1sums" >&2
+		return 1
+	fi
+	return 0
 }
+
+tsp_create_env()
+{
+	TS_TMPDIR="`basename "${testfile}"`.tmpd"
+	rm -rf "${TS_TMPDIR}" || return 1
+	mkdir "${TS_TMPDIR}" || return 1
+
+	TS_STDIN="${TS_TMPDIR}/stdin"
+	TS_EXP_STDOUT="${TS_TMPDIR}/exp_stdout"
+	TS_EXP_STDERR="${TS_TMPDIR}/exp_stderr"
+	TS_OUTFILE="${TS_TMPDIR}/tool_outfile"
+	TS_EXP_EXIT_CODE="0"
+	TS_DIFF_OPTS=""
+
+	tool_stdout="${TS_TMPDIR}/tool_stdout"
+	tool_stderr="${TS_TMPDIR}/tool_sterr"
+}
+
+myexit()
+{
+	if test "${1}" = "0"; then
+		rm -rf "${TS_TMPDIR}"
+	fi
+	exit ${1:-1}
+}
+
+## setup
+fail=0
+tsp_create_env || myexit 1
+
+## also set srcdir in case the testfile needs it
+if test -z "${srcdir}"; then
+	srcdir=`xrealpath \`dirname "${0}"\``
+else
+	srcdir=`xrealpath "${srcdir}"`
+fi
+
+## source the check
+. "${testfile}" || myexit 1
 
 eval_echo()
 {
@@ -126,7 +146,7 @@ eval_echo()
 		echo >&3
 	else
 		echo "<<EOF" >&3
-		tmpf=$(mktemp "/tmp/tmp.XXXXXXXXXX")
+		tmpf=`mktemp "/tmp/tmp.XXXXXXXXXX"`
 		tee "${tmpf}" >&3
 		echo "EOF" >&3
 	fi
@@ -145,30 +165,31 @@ fi
 
 ## set finals
 if test -x "${builddir}/${TOOL}"; then
-	TOOL=$(xrealpath "${builddir}/${TOOL}")
-fi
-if test -z "${srcdir}"; then
-	srcdir=$(xrealpath $(dirname "${0}"))
-else
-	srcdir=$(xrealpath "${srcdir}")
+	TOOL=`xrealpath "${builddir}/${TOOL}"`
 fi
 
-stdin=$(find_file "${stdin}")
-stdout=$(find_file "${stdout}")
-stderr=$(find_file "${stderr}")
+stdin=""
+if test -r "${TS_STDIN}"; then
+	stdin="${TS_STDIN}"
+fi
+stdout="${TS_EXP_STDOUT}"
+stderr="${TS_EXP_STDERR}"
 
-eval_echo "${HUSK}" "${TOOL}" "${CMDLINE}" \
+eval_echo "${husk}" "${TOOL}" "${CMDLINE}" \
 	< "${stdin:-/dev/null}" \
 	3>&2 \
-	> "${tool_stdout}" 2> "${tool_stderr}" || fail=${?}
+	> "${tool_stdout}" 2> "${tool_stderr}"
+tool_exit_code=${?}
 
 echo
-if test "${EXPECT_EXIT_CODE}" = "${fail}"; then
-	fail=0
+if test "${TS_EXP_EXIT_CODE}" != "${tool_exit_code}"; then
+	echo "test exit code was ${tool_exit_code} (expected: ${TS_EXP_EXIT_CODE})"
+	fail=1
+	echo
 fi
 
 if test -r "${stdout}"; then
-	diff -u "${stdout}" "${tool_stdout}" || fail=1
+	eval diff -u "${TS_DIFF_OPTS}" "${stdout}" "${tool_stdout}" || fail=1
 elif test -s "${tool_stdout}"; then
 	echo
 	echo "test stdout was:"
@@ -176,7 +197,7 @@ elif test -s "${tool_stdout}"; then
 	echo
 fi
 if test -r "${stderr}"; then
-	diff -u "${stderr}" "${tool_stderr}" || fail=1
+	eval diff -u "${TS_DIFF_OPTS}" "${stderr}" "${tool_stderr}" || fail=1
 elif test -s "${tool_stderr}"; then
 	echo
 	echo "test stderr was:"
@@ -184,6 +205,21 @@ elif test -s "${tool_stderr}"; then
 	echo
 fi
 
+## check if we need to hash stuff
+if test -n "${TS_OUTFILE_SHA1}"; then
+	if sum="`ts_sha1sum "${TS_OUTFILE}"`"; then
+		if test "${sum}" != "${TS_OUTFILE_SHA1}"; then
+			cat <<EOF >&2
+outfile (${TS_OUTFILE}) hashes do not match:
+SHOULD BE: ${TS_OUTFILE_SHA1}
+ACTUAL:    ${sum}
+EOF
+		fail=1
+		fi
+	else
+		fail=1
+	fi
+fi
+
 myexit ${fail}
 
-## tws-test.sh ends here
