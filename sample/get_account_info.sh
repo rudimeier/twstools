1adf2e61bfa3eef62bebde52654f04d02b0a577c
Ruediger Meier <ruediger.meier@ga-group.nl>
2013-05-29 17:48:20 +0200

    style, remove trailing white spaces from other files
    used command: sed -i 's/[\t ]*$//g' $(git ls-files)

--- sample/get_account_info.sh
+++ sample/get_account_info.sh
@@ -4,7 +4,7 @@
 TWSDO="twsdo -h localhost -p 7496"
 
 # get some contract details
-${TWSDO} --get-account > out_account.xml 
+${TWSDO} --get-account > out_account.xml
 ${TWSDO} --get-exec > out_exec.xml
 
 
