ae6bda291294d306c48d2803d846c8745215f212
Ruediger Meier <ruediger.meier@ga-group.nl>
2014-04-27 09:22:53 +0200

    remove assert in remove_last_request()

--- src/tws_meta.cpp
+++ src/tws_meta.cpp
@@ -1412,7 +1412,10 @@ void PacingControl::addRequest()
 
 void PacingControl::remove_last_request()
 {
-	assert(dateTimes.size() > 0 && violations.size() > 0 );
+	//assert(dateTimes.size() > 0 && violations.size() > 0 );
+	if(dateTimes.size() <= 0 || violations.size() <= 0 ) {
+		DEBUG_PRINTF( "Warning, assert remove_last_request");
+	}
 	dateTimes.pop_back();
 	violations.pop_back();
 }
