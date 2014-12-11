rudimeier/twstools

commit ae6bda291294d306c48d2803d846c8745215f212

remove assert in remove_last_request()

src/tws_meta.cpp
@@ -1412,7 +1412,10 @@ void PacingControl::addRequest()
 
 void PacingControl::remove_last_request()
 {
-	assert(dateTimes.size() > 0 && violations.size() > 0 );
+	//assert(dateTimes.size() > 0 && violations.size() > 0 );
+	if(dateTimes.size() <= 0 || violations.size() <= 0 ) {
+		DEBUG_PRINTF( "Warning, assert remove_last_request");
+	}
 	dateTimes.pop_back();
 	violations.pop_back();
 }

commit 8cb7f03c62c42e7bf734b6c8c4d415f54e45e333

fix, remove_last_request list corruption
Introduced in ae6bda29.

src/tws_meta.cpp
@@ -1480,9 +1480,9 @@ void PacingControl::addRequest()
 
 void PacingControl::remove_last_request()
 {
-	//assert(dateTimes.size() > 0 && violations.size() > 0 );
-	if(dateTimes.size() <= 0 || violations.size() <= 0 ) {
+	if( dateTimes.size() <= 0 ) {
 		DEBUG_PRINTF( "Warning, assert remove_last_request");
+		return;
 	}
 	dateTimes.pop_back();
 	violations.pop_back();